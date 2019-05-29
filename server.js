e20ac7698ebd083ffb5455b22eef57f9640491f8
harsh0907 <harshagarwal.network@gmail.com>
2019-05-29 20:47:21 +0530

    add database

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
 const express = require('express')
+const mongooes = require('mongoose')
 
 const app = express()
 
+
+mongooes.connect('mongodb://127.0.0.1:27017/task-manager-api',{
+    useNewUrlParser: true,
+    useCreateIndex: true
+})
+
+
+
+const user = mongooes.model('user',{
+    name:{
+        type:String
+    },
+    age:{
+        type:Number
+    }
+})
+
+
+
+
+
+
+
 app.get('/' , (req,res)=>{
+    new user({name:"harsh",age:20})
+    .save().then(res => res.send(res))
     res.send("<h1>harsh</h1>")
 })
 
 app.get('/about', (req,res)=>{
-    res.send("<h1>About</h1>")
+    console.log(user.find({}))
 })
 
 app.listen(3000,()=>{
